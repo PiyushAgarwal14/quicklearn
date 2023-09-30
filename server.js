1def0fc7076a91f4e80f487e69ce370aead03582
Piyush Agarwal <87847452+PiyushAgarwal14@users.noreply.github.com>
2023-09-30 18:24:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
   port: 587,
   auth: {
-    user: process.env.EMAIL,
-    pass: process.env.PASS,
+    user: 'examtimertu@gmail.com',
+    pass: 'gxvdiwiznxohcdyg',
   },
 });
 
